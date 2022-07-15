6b4308e9e5cc78694095e0365c6648058a9e81a7
Renato Marques Peçanha Araújo <nartain@gmail.com>
2022-07-14 21:55:41 -0300

    Pensando coisinhas

---
+++ modulo3/thinking-react/mockdados/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <projeto>
+      <header />
+      <body>
+        <filtros></filtros>
+        <produtos></produtos>
+        <carrinho></carrinho>
+      </body>
+      <footer />
+    </projeto>
+  );
+}
+
+export default App;
