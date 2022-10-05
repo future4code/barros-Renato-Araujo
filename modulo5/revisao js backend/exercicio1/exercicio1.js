220330dbe8823174beb7f5012db763a42b0e04f7
Renato Marques Peçanha Araújo <--global>
2022-10-04 22:20:31 -0300

    feito

---
+++ modulo5/revisao js backend/exercicio1/exercicio1.js
@@ -0,0 +1,23 @@
+const converteCelsius = (temp, type) => {
+    if (isNaN(temp)) 
+    {console.log(`“Erro. Parâmetro inválido (${temp} não é um número válido!).`)}
+
+    else {if  (type === type.toLowerCase()) 
+        {console.log(`“Erro. Parâmetro inválido ${type} deve ser escrita com maiúscula!).`)
+        
+        
+        if ((type !== "f" || type !=="k") && type === type.toUpperCase()) 
+        {console.log(`“Erro. Parâmetro inválido ${type} não é uma temperatura válida!).`)}}
+
+        
+
+       
+    else {console.log(`${temp}° ${type} equivalem a 86° Farenheit!`)}}}
+    
+
+
+converteCelsius("trinta", "F")
+converteCelsius(30, "G")
+converteCelsius("30", "k")
+converteCelsius(30, "F")
+converteCelsius(303, "K")
