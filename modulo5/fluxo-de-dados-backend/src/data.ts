ab469a833108c1b14f5eefa209be7b376a5dbceb
Renato Marques Peçanha Araújo <--global>
2022-11-01 22:35:58 -0300

    Abri mão do simples para fazer um menu interativo com userInput.question e me enrolei todo. Queimei os neurônios nisso, mas tudo bem. Amanhã, vou repassar algumas coisas, refazer o feijão com arroz. Paz!

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,29 @@
+import * as allTypes from "./types"
+
+export const data: allTypes.Products[] = [
+    {
+        id: '1667334185048',
+        name: 'Dafra Roadwin 250r',
+        price: 8210
+    },
+    {
+        id: '1667334199552',
+        name: 'Kawasaki Ninja 300',
+        price: 20590
+    },
+    {
+        id: '1667334208063',
+        name: 'Honda PCX 150 ABS',
+        price: 13420
+    },
+    {
+        id: '1667334216269',
+        name: 'Kawasaki Vulcan 650s',
+        price: 28220
+    },
+    {
+        id: '1667334222915',
+        name: 'Honda PCX 160 CBS',
+        price: 17000
+    }
+]
